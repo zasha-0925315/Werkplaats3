8d64ea8b51b4b1f3f79f28dffe187c390a58a6e9
Sushi <31491170+0974201@users.noreply.github.com>
2023-02-15 14:32:57 +0100

    added sum classes

---
+++ lib/manageteacher.py
@@ -0,0 +1,26 @@
+import os
+import sqlite3
+from sqlite3 import OperationalError
+
+
+class TeacherManagement:
+    """regelt de users enzo"""
+
+    def __init__(self, db_file):
+        self.db_file = db_file
+        if not os.path.exists(self.db_file):
+            raise FileNotFoundError(f"F in the chat for {db_file}")
+
+    def get_teacher(self):
+        try:
+            conn = sqlite3.connect()
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM docent")
+            conn.commit() 
+
+            conn.close()
+
+        except OperationalError as e:
+            print("yeet")
+            raise e
