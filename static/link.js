99e1a1c5441f6d7467d96daa918ab2a4be511248
Yong Pok Low <1053079@hr.nl>
2023-02-18 21:28:33 +0100

    testing the javascript script is working

---
+++ static/link.js
@@ -0,0 +1,8 @@
+document.getElementsByName("meeting1")[0].placeholder = "Empty";
+document.getElementsByName("meeting2")[0].placeholder = "Empty";
+document.getElementsByName("meeting3")[0].placeholder = "Empty";
+
+function Clickme() {
+    text = "Shin Hadouken";
+    document.getElementById("Greet-Balk").innerHTML = text;
+}
