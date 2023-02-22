464252dbf67851868a68d29359f6d9f29eb57bfc
Zasha <0925315@hr.nl>
2023-02-22 20:58:23 +0100

    kan informatie op halen van een api. Hoe verwerk ik dit? Geen idee

--- static/meeting.js
+++ static/meeting.js
@@ -1,8 +1,8 @@
 const get_meeting = async () => {
     try {
         const  response = await fetch('https://xivapi.com/character/27782546', {
-            method: 'POST',
-            body: "some body",
+            method: 'GET',
+            // body: "some body",
             headers: {
                 'Content-type': 'application/json'
             }
@@ -10,21 +10,25 @@ const get_meeting = async () => {
         console.log(response)
         const data = await response.json();
         console.log(data)
-        // if (response.error) {
-        //     console.log("oopsie")
-        // } else if (!response.ok) {
-        //     console.log("Some non-200 HTTP responmse code")
-        // } else {
-        //     if (data["meeting"].length > 0) {
-        //         const meetingInfo = document.querySelector("#test_div")
-        //         meetingInfo.replaceChildren()
-        //         data["meeting"].forEach(function (item) {
-        //             meetingInfo.innerHTML +="<dt>" + item["name"] + "</dt><dd>" + item["location"] + "</dd>"
-        //         })
-        //     } else {
-        //         console.log("hi")
-        //     }
-        // }
+        if (response.error) {
+            console.log("oopsie")
+        } else if (!response.ok) {
+            console.log("Some non-200 HTTP responmse code")
+        } else {
+            if (data.length > 0) {
+                const meetingInfo = document.querySelector("#test_div")
+                meetingInfo.replaceChildren()
+                data.forEach(function (item) {
+                    meetingInfo.innerHTML +="<dt>" + item["name"] + "</dt><dd>" + item["location"] + "</dd>"
+                })
+            } else {
+                const parsejson = JSON.parse(data)
+                console.log(parsejson["Minions"])
+                const meetingInfo = document.querySelector("#test_div")
+                meetingInfo.replaceChildren()
+                meetingInfo.innerHTML = parsejson
+            }
+        }
     } catch (e) {
         console.log("Some error with fetching JSON from meeting server: " + e)
     } finally {
