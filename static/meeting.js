483411fe53169204199594c89772db13b054cb1d
Zasha <0925315@hr.nl>
2023-02-23 11:49:27 +0100

    kan informatie op halen van een api. kan nu ook de informatie laten zien op het scherm

--- static/meeting.js
+++ static/meeting.js
@@ -1,6 +1,6 @@
 const get_meeting = async () => {
     try {
-        const  response = await fetch('https://xivapi.com/character/27782546', {
+        const  response = await fetch('https://ffxivcollect.com/api/mounts/186', {
             method: 'GET',
             // body: "some body",
             headers: {
@@ -13,7 +13,7 @@ const get_meeting = async () => {
         if (response.error) {
             console.log("oopsie")
         } else if (!response.ok) {
-            console.log("Some non-200 HTTP responmse code")
+            console.log("Some non-200 HTTP response code")
         } else {
             if (data.length > 0) {
                 const meetingInfo = document.querySelector("#test_div")
@@ -22,11 +22,11 @@ const get_meeting = async () => {
                     meetingInfo.innerHTML +="<dt>" + item["name"] + "</dt><dd>" + item["location"] + "</dd>"
                 })
             } else {
-                const parsejson = JSON.parse(data)
-                console.log(parsejson["Minions"])
+                const strjson = JSON.stringify(data["name"])
+                console.log(strjson)
                 const meetingInfo = document.querySelector("#test_div")
                 meetingInfo.replaceChildren()
-                meetingInfo.innerHTML = parsejson
+                meetingInfo.innerHTML = strjson
             }
         }
     } catch (e) {
