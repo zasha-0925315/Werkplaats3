6281553346635d0354fb488303c6dc8b864c1db0
Yong Pok Low <1053079@hr.nl>
2023-03-02 03:49:03 +0100

    undefined?? test test

--- static/link.js
+++ static/link.js
@@ -1,83 +1,34 @@
-const url = ('https://restcountries.com/v3.1/all')
 
 
-fetch(url)
-    .then(response => response.json())
-    .then(repos => {
-        const reposList = repos.map(repo => repo.name);
-        console.log(reposList);
-    })
-    .catch(err => console.log(err))
-
-function set_Meeting() {
-    meeting = document.querySelector('tbody')
-    meeting.innerHTML = country.name
-}
-
-const fetch_scores = async () => {
+const get_meeting = async () => {
     try {
-        const response = await fetch(url, {
+        const url = window.location.pathname.split('/')
+        const urlId = [1]
+        const response = await fetch('../api/class/' + urlId, {
             method: 'GET',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-type': 'application/json'
             }
         });
+
         const data = await response.json();
         if (response.error) {
-            console.log('errorr')
+            console.log("ouwh error")
         } else if (!response.ok) {
-            console.log('some non-200 HTTP response code')
+            console.log("Some non-200 HTTP response code or something")
         } else {
-            set_meeting(data)
+            if (data["meeting_info"].length > 0) {
+                const table = document.getElementById("Docent-Table")
+                const tbody = document.querySelector("tbody")
+                table.innerHTML += data['date']
+            }
+
         }
     } catch (e) {
-        console.log("SOME ERROR WITH FETCHING FROM REPO: " + e)
+        console.log("Some error with fetching JSON from Meetings server: " + e)
     }
 }
 
-fetch_scores()
+get_meeting()
 
-let interval_id = setInterval(fetch_scores, 5000)
-// const SERVER_URL = ("https://restcountries.com/v3.1/all")
-
-// function add_Country() {
-//     let countries = JSON.parse(SERVER_URL.response)
-//     console.log(countries)
-//     document.getElementById('Docent-Planning').innerHTML = country.name
-// }
-
-// // function set_countries(countrydata) {
-// //     let countrylist = document.getElementById("Docent-Planning")
-// //     countrylist.replaceChildren()
-// //     if (countrydata["country"].length > 0) {
-// //         countrydata["country"].forEach(function (item) {
-// //             countrylist.innerHTML += "<dt>" + item["name"] + "</dt><dd>" + item["country"] + "</dd>"
-// //         })
-// //     }
-// // }
-
-// const fetch_countries = async () => {
-//     try {
-//         const response = await fetch(SERVER_URL, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         });
-//         const data = await response.json();
-//         if (response.error) {
-//             console.log("Countries error!!")
-//         } else if (!response.ok) {
-//             console.log("Some non-200 HTTP code")
-//         } else {
-//             set_countries(data)
-//         }
-//     } catch (e) {
-//         console.log("Some error with fetching JSON from api: " + e)
-//     }
-// }
-
-// fetch_countries()
-
-// // Note that setInterval requires a function reference, not a variable!
-// let interval_id = setInterval(fetch_countries, 5000)
+let interval_id = setInterval(get_meeting, 5000)
