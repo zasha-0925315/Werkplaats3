04f9c3c86a3106da129b1cf3dd93b55948b2b9d8
Sushi <31491170+0974201@users.noreply.github.com>
2023-03-04 04:01:24 +0100

    hoe krijg ik die shit in de tabel tho
    Signed-off-by: Sushi <31491170+0974201@users.noreply.github.com>

--- static/student.js
+++ static/student.js
@@ -2,8 +2,9 @@ async function get_students(){
     try {
         const response = await fetch('../api/student');
         const student = await response.json();
+        
         console.log(`${JSON.stringify(student)}`);
-        return student;
+
     } catch(error) {
         console.log(error);
     }
