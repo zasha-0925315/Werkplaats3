49b3f3c625e317f57e07006068b5655dff7f8cf2
Sushi <31491170+0974201@users.noreply.github.com>
2023-03-04 23:21:04 +0100

    this ain't it, chief. hij vult de tabel nu wel tho
    Signed-off-by: Sushi <31491170+0974201@users.noreply.github.com>

--- static/js/student.js
+++ static/js/student.js
@@ -5,8 +5,40 @@ async function get_students(){
         
         console.log(`${JSON.stringify(student)}`);
 
+        fill_table(student);
+
     } catch(error) {
         console.log(error);
+    }
+}
+
+function fill_table(obj){
+    const table = document.getElementById('table');
+    const students = obj.studenten;
+    
+    for(const student of students){
+        
+        const tb = document.createElement('tbody');
+        let tr = document.createElement('tr');
+        tr.innerHTML = '<td>' + student.id + '</td>'
+        + '<td>' + student.voornaam + '</td>' +
+        '<td>' + student.achternaam + '</td>';
+        tb.appendChild(tr);
+        table.appendChild(tb);
+        
+        //const snr = obj.id;
+        //const voornaam = obj.voornaam;
+        //const achternaam = obj.achternaam;
+        //const tr = document.createElement('tr');
+        //const th = document.createElement('th');
+        //th.textContent = "studentennummer";
+        //table.appendChild(th);
+        //const td = document.createElement('td');
+
+        
+        //tr.textContent = student.id;
+        
+        //table.appendChild(tr);
     }
 }
 
