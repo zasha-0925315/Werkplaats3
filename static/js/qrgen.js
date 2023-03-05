2a1a992f61ba3ea51a7750ce88dff49f3417c74a
Sushi <31491170+0974201@users.noreply.github.com>
2023-03-05 02:14:43 +0100

    inline js in aparte js files gezet
    Signed-off-by: Sushi <31491170+0974201@users.noreply.github.com>

---
+++ static/js/qrgen.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector("#qr-gen");
+
+function generateCode(){
+    let getText = document.getElementById("text").value;
+    
+    if(getText){ //laat pas de QR code zien als er daadwerkelijk text is ingevoerd
+        let qr = new QRious({
+            foreground: "#D30F4C",
+            background: "#FFFFFF",
+            backgroundAlpha: 0.1,
+            size: 500,
+            level: "H",
+            padding: 25,
+            element: document.getElementById("qr"),
+            value: getText
+        })
+    } else {
+        alert("Geen text ingevoerd!");
+    }
+}
+
+btn.addEventListener('click', generateCode);
