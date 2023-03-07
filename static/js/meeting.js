fd7cb99262568bccadc5817d9d52195734e98d89
Zasha <0925315@hr.nl>
2023-03-07 23:45:07 +0100

    voor een 2de merge want somigge dingen waren verdewen na een andere merge

--- static/js/meeting.js
+++ static/js/meeting.js
@@ -20,18 +20,28 @@ const get_meeting = async () => {
                 const presence_length = data["presence_list"].length
                 let student_count = 0
                 let student_presence = 0
-
+                console.log(data["presence_list"])
                 meetingInfo.replaceChildren()
                 while ( student_count < presence_length) {
+
                     let afgemeld_reason = data["presence_list"][student_count]["afgemeld reason"]
                     if (afgemeld_reason === null) {
                         afgemeld_reason = "Geen reden"
                     }
+
+                    let student_check_in_time = "<td>" + data["presence_list"][student_count]["check-in time"] + "</td>"
+                    console.log(data["presence_list"][student_count]["check-in time"])
+                    if (data["presence_list"][student_count]["check-in time"] === null) {
+                        console.log("why")
+                        student_check_in_time = "<td></td>"
+                    }
+
                     let student_name = "<td>" + data["presence_list"][student_count]["first name"] + " " + data["presence_list"][student_count]["last name"] + "</td>"
                     let student_afwezig = "<td class='no_presence'>Afwezig</td>"
                     let student_aanwezig = "<td class='yes_presence'>Aanwezig</td>"
                     let student_afgemeld = "<td class='maybe_presence'>Afgemeld</td>"
                     let student_error = "<td>" + "error" + "</td>"
+
                     let presence_options =
                         "<td class='presence_options'>" +
                         "<div id='button_presence_yes' class='button_presence' data-value='1' data-count=" + student_count + ">:)</div>" +
@@ -41,14 +51,14 @@ const get_meeting = async () => {
 
                     switch (data["presence_list"][student_count]["presence"]) {
                         case 0:
-                            meetingInfo.innerHTML += student_name + student_afwezig + presence_options
+                            meetingInfo.innerHTML += student_name + student_afwezig + student_check_in_time + presence_options
                             break
                         case 1:
-                            meetingInfo.innerHTML += student_name + student_aanwezig + presence_options
+                            meetingInfo.innerHTML += student_name + student_aanwezig + student_check_in_time + presence_options
                             student_presence++
                             break
                         case 2:
-                            meetingInfo.innerHTML += "<tr class='afgemeld_row'>" + student_name + student_afgemeld + presence_options +
+                            meetingInfo.innerHTML += "<tr class='afgemeld_row'>" + student_name + student_afgemeld + student_check_in_time + presence_options +
                                "<td class='afgemeld_reason'>" + afgemeld_reason + "</td>" + "</tr>"
                             break
                         default:
@@ -94,7 +104,7 @@ const get_meeting = async () => {
     } catch (e) {
         console.log("Some error with fetching JSON from meeting server: " + e)
     } finally {
-        setTimeout(get_meeting, 5000)
+        setTimeout(get_meeting, 5000000)
     }
 }
 document.addEventListener('DOMContentLoaded', get_meeting)
