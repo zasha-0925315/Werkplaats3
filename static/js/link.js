8ac0cf1640680971b261bf8c42c28961f0152573
Zasha <0925315@hr.nl>
2023-03-16 21:10:18 +0100

    bezig met het filtreren op leraar

--- static/js/link.js
+++ static/js/link.js
@@ -2,40 +2,19 @@ const get_meeting = async () => {
     try {
         const response = await fetch('../api/class/json');
         const data = await response.json();
-        const meeting_array = data["meeting_info"]
+        const meetingArray = data["meeting_info"]
 
-        meeting_array.sort((a, b) => {
+        meetingArray.sort((a, b) => {
             if (a.date < b.date) {
                 return -1
             }
         })
-
         if (response.error) {
             console.log("ouwh error")
         } else if (!response.ok) {
             console.log("Some non-200 HTTP response code or something")
         } else {
-
-
-            if (meeting_array.length > 0) {
-                const tbody = document.querySelector("#docent-body")
-                let planning_length = 0
-                let max_length = 3
-                let array_length = meeting_array.length
-                let currentDate = new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf("T"));
-                console.log("c:"+currentDate)
-                console.log(meeting_array)
-                tbody.replaceChildren()
-                while (planning_length < array_length && planning_length < max_length) {
-                    if (meeting_array[planning_length]["date"] <= currentDate) {
-                        meeting_array.splice(planning_length, 1)
-                        console.log(meeting_array)
-                    }
-                    console.log("t:"+meeting_array[planning_length]["date"])
-                    tbody.innerHTML += " <td><strong> " + meeting_array[planning_length]["name"] + "</strong> <small>" + meeting_array[planning_length]["date"] + "</small></td>";
-                    planning_length++
-                }
-            }
+            selectTeacher(meetingArray)
         }
     }
     catch (e) {
@@ -43,16 +22,56 @@ const get_meeting = async () => {
     }
 }
 
+function filterTeacher(meetingArray, teacher){
+    return meetingArray["teacher"] === teacher
+}
 
-document.querySelector("#Select-Teacher").addEventListener("change", function () {
-    if (this.value == "MEYEF") {
-        console.log("MEYEF");
-    } else if (this.value == "KNUFI") {
-        console.log("KNUFI");
-    } else {
-        console.log('dog')
+function selectTeacher(meetingArray){
+    document.querySelector("#Select-Teacher").addEventListener("change", function () {
+        let teacherArray = String(meetingArray["teacher"])
+            .replaceAll(" ", "")
+            .replaceAll("'", "")
+            .replace("[", "")
+            .replace("]", "")
+            .split(",")
+
+        let filteredTeacher = meetingArray.filter(meetingArray["teacher"] = this.value.replace("[", ""))
+        console.log(teacherArray)
+        return teacherArray = this.value
+    });
+}
+
+function creatTable(meetingArray){
+    if (meetingArray.length > 0) {
+        const tbody = document.querySelector("#docent-body")
+        let currentDate = new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf("T"));
+        let dateFiltered = meetingArray.filter(filterDate)
+        let planningLength = 0
+        let maxLength = 3
+        let arrayLength = dateFiltered.length
+
+
+        tbody.replaceChildren()
+
+        while (planningLength < arrayLength && planningLength < maxLength) {
+                meetingArray.splice(planningLength, 1)
+              tbody.innerHTML +=
+                "<td><strong> " + dateFiltered[planningLength]["name"] + "</strong></td>" + " " +
+                "<td><small>" + dateFiltered[planningLength]["date"] + "</small></td>" + " " +
+                "<td><small>" + dateFiltered[planningLength]["start_time"] + "</small></td>" + " " +
+                "<td><small>" + dateFiltered[planningLength]["end_time"] + "</small></td>"
+
+            planningLength++
+        }
+        function filterDate(meetingArray) {
+            return meetingArray["date"] >= currentDate
+        }
     }
-});
+}
+
+
+
+
 
 function dropMenu() {
     document.getElementById('menu').style.display = 'initial'
