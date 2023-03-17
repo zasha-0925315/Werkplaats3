2cf2bde419e110ea98efc22510e72d74d1896186
Zasha <0925315@hr.nl>
2023-03-17 13:02:54 +0100

    kan filtreren op docent

--- static/js/link.js
+++ static/js/link.js
@@ -1,4 +1,5 @@
-const get_meeting = async () => {
+// Get the list of meetings
+async function getMeeting () {
     try {
         const response = await fetch('../api/class/json');
         const data = await response.json();
@@ -9,39 +10,26 @@ const get_meeting = async () => {
                 return -1
             }
         })
-        if (response.error) {
-            console.log("ouwh error")
-        } else if (!response.ok) {
-            console.log("Some non-200 HTTP response code or something")
-        } else {
-            selectTeacher(meetingArray)
-        }
+
+        selectTeacher(meetingArray)
+
     }
     catch (e) {
         console.log("Some error with fetching JSON from Meetings server: " + e)
     }
 }
+// select the <select>
+const teacherSelectBox = document.querySelector("#Select-Teacher")
 
-function filterTeacher(meetingArray, teacher){
-    return meetingArray["teacher"] === teacher
-}
-
+// add an eventlistenter to the change of the <select>
 function selectTeacher(meetingArray){
-    document.querySelector("#Select-Teacher").addEventListener("change", function () {
-        let teacherArray = String(meetingArray["teacher"])
-            .replaceAll(" ", "")
-            .replaceAll("'", "")
-            .replace("[", "")
-            .replace("]", "")
-            .split(",")
-
-        let filteredTeacher = meetingArray.filter(meetingArray["teacher"] = this.value.replace("[", ""))
-        console.log(teacherArray)
-        return teacherArray = this.value
+    teacherSelectBox.addEventListener("change", function () {
+        createTable(meetingArray)
     });
 }
 
-function creatTable(meetingArray){
+// create the table
+function createTable(meetingArray){
     if (meetingArray.length > 0) {
         const tbody = document.querySelector("#docent-body")
         let currentDate = new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf("T"));
@@ -50,52 +38,31 @@ function creatTable(meetingArray){
         let maxLength = 3
         let arrayLength = dateFiltered.length
 
-
+        console.log(teacherSelectBox.value)
         tbody.replaceChildren()
 
         while (planningLength < arrayLength && planningLength < maxLength) {
-                meetingArray.splice(planningLength, 1)
-              tbody.innerHTML +=
-                "<td><strong> " + dateFiltered[planningLength]["name"] + "</strong></td>" + " " +
-                "<td><small>" + dateFiltered[planningLength]["date"] + "</small></td>" + " " +
-                "<td><small>" + dateFiltered[planningLength]["start_time"] + "</small></td>" + " " +
-                "<td><small>" + dateFiltered[planningLength]["end_time"] + "</small></td>"
-
+            if (dateFiltered[planningLength]["teacher"].includes(teacherSelectBox.value)){
+                console.log(dateFiltered[planningLength]["teacher"])
+                    tbody.innerHTML +=
+                    "<td><strong> " + dateFiltered[planningLength]["name"] + "</strong></td>" + " " +
+                    "<td><small>" + dateFiltered[planningLength]["date"] + "</small></td>" + " " +
+                    "<td><small>" + dateFiltered[planningLength]["start_time"] + "</small></td>" + " " +
+                    "<td><small>" + dateFiltered[planningLength]["end_time"] + "</small></td>"
+            }
             planningLength++
+        }
+        if (tbody.innerHTML === ""){
+            tbody.innerHTML = "<td>Geen aankonde bijeenkomsten</td>"
         }
         function filterDate(meetingArray) {
             return meetingArray["date"] >= currentDate
         }
+    }else{
+        console.log("kio")
     }
 }
 
+// activate the getMeeting function when the DOM is loaded
+document.addEventListener('DOMContentLoaded', getMeeting())
 
-
-
-
-function dropMenu() {
-    document.getElementById('menu').style.display = 'initial'
-}
-
-// window.onclick = function (event) {
-//     if (!event.target.matches('.Select-Planning-button')) {
-//         let dropdowns = document.getElementsByClassName("menu");
-//         let i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.style.display = 'initial') {
-//                 (openDropdown.style.display = 'none')
-//             }
-//         }
-//     }
-// }
-
-// lijst = document.querySelector('#Select-Teacher')
-// lijst.addEventListener("change", docentLijst()); {
-
-// };
-
-
-get_meeting()
-
-let interval_id = setInterval(get_meeting, 5000)
