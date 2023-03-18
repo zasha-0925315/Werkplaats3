e2fda184ca448cb26d226c62ead8e035ea957b75
Yong Pok Low <1053079@hr.nl>
2023-03-18 01:13:00 +0100

    redirects student after check-in

--- static/js/checkin.js
+++ static/js/checkin.js
@@ -4,11 +4,13 @@ const button = document.querySelector("#button_checkin")
 const url = window.location.pathname.split('/')
 const urlId = url[2]
 
+// error message if student does not input the right info //
 function displayMessage() {
-    message = 'ongeldige studentenid'
+    message = 'Ongeldige studentenid'
     document.querySelector('#message').innerHTML = message;
 }
 
+// fetch question from meeting // 
 function fetchQuestion() {
     fetch('/checkin/' + urlId, {
         method: 'POST',
@@ -23,6 +25,8 @@ function fetchQuestion() {
     })
 }
 
+/* checks if student has put in 7 characters..
+displays an error message if the student has not. */
 
 button.addEventListener('click', function () {
     let newTime = new Date().toLocaleTimeString('nl-nl')
@@ -33,16 +37,24 @@ button.addEventListener('click', function () {
                 'presence': 1,
                 'meeting': urlId,
                 'student': studentId.value,
-                'checkin time': newTime
+                'checkin time': newTime,
             }),
             headers: {
                 'Content-type': 'application/json'
             }
-        })
+        }) // if json information is processed then this redirects user to the href link //
+            .then(response => {
+                if (response.ok) {
+                    window.location.href = '/meeting';
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        //  checks for whether there was a question submitted by a teacher in meeting //
         if (question.value !== "") {
             fetchQuestion()
         }
-
     }
     else {
         displayMessage()
